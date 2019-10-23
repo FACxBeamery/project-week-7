75ad47e6d9a3ef06bb1282248c3cb0f355fd3e79
Lyndsey Scott <lyndsey.736@gmail.com>
2019-10-23 16:57:33 +0100

    fns add categories and topics to page

--- src/components/mainContent/mainContent.js
+++ src/components/mainContent/mainContent.js
@@ -1,8 +1,51 @@
 import React from "react";
 import styles from "./mainContent.module.css";
+import initialDataTopics from "../../initialDataTopics";
+const categories = ["frontend", "backend", "people", "non work"];
+const locations = ["London", "San Francisco", "Austin"];
 
-const MainContent = () => {
-	return <div></div>;
+const CategoryCard = ({ allCategories }) => {
+	return allCategories.map((category) => (
+		<div className="category-card">{category}</div>
+	));
+
+	// for each item in category to render a card with the category
 };
 
+const TopicCard = ({ allTopics }) => {
+	return allTopics.map((obj) => (
+		<div className="topic-card">
+			<p>{obj.topic}</p>
+		</div>
+	));
+};
+
+const MainContent = ({
+	category,
+	setCategory,
+	topic,
+	setTopic,
+	location,
+	setLocation,
+	person,
+	setPerson
+}) => {
+	return (
+		<div>
+			<CategoryCard allCategories={categories}></CategoryCard>
+			{/* if condition for filtering for topics<TopicCard allTopics={topics}></TopicCard> */}
+		</div>
+	);
+};
+
+// const Option = ({ options }) => {
+// 	const initialArray = [
+// 		<option hidden disabled selected value>
+// 			All
+// 		</option>
+// 	];
+// 	return initialArray.concat(
+// 		options.map((option) => <option value={option}>{option}</option>)
+// 	);
+// };
 export default MainContent;
