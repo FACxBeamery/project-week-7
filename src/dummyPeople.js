3d8dce003984192f9d4ba1a97de3b2db2bb0544a
Martha Lambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-10-24 13:46:05 +0100

    dummy data fix

--- src/dummyPeople.js
+++ src/dummyPeople.js
@@ -1,11 +1,9 @@
 const ObjectID = require("mongodb").ObjectID;
-​
 const initialDataPeople = [
 	{
 		_id: ObjectID("666f6f2d6261722d71757578"),
 		name: "Lyndsey Scott",
 		job: "Graduate Software Engineer",
-​
 		topics: [
 			ObjectID("507f1f77bcf86cd799439011"),
 			ObjectID("54759eb3c090d83494e2d804")
@@ -18,5 +16,4 @@ const initialDataPeople = [
 		topics: [ObjectID("54759eb3c090d83494e2d804")]
 	}
 ];
-​
 module.exports = initialDataPeople;
