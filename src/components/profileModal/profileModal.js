8e33df828861c925b36724f4f8ad3cbe7a362f9d
Lyndsey Scott <lyndsey.736@gmail.com>
2019-10-24 15:03:21 +0100

    modal with styling

--- src/components/profileModal/profileModal.js
+++ src/components/profileModal/profileModal.js
@@ -1,13 +1,20 @@
 import React from "react";
 import styles from "./profileModal.module.css";
-
+// `${styles["first--form--input"]} ${
+// 	styles["first--form--input--error"]
+// }`
 const profileModal = ({ profile }) => {
 	/// we will need an extra argument for returning the interests
 	return (
-		<div className={styles["modal"]}>
+		<div className={`${styles["modal"]} ${styles["modal-content"]}`}>
 			<p>{profile.name}</p>
 			<p>{profile.job}</p>
 			<p>{profile.office}</p>
+			<div className={styles["modal-interests"]}>
+				<p>Eating Out</p>
+				<p>Saying That's Wild</p>
+			</div>
+			{/* <p>{profile.topics}</p> */}
 			{/* also here we need to have the interests but after we have done some backend stuff to filter that */}
 		</div>
 	);
