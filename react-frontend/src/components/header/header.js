907174fed71b71823c767ab16702da518ff1d074
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-11-05 10:26:52 +0000

    header merge fix

--- react-frontend/src/components/header/header.js
+++ react-frontend/src/components/header/header.js
@@ -5,108 +5,109 @@ import Popup from "reactjs-popup";
 
 import profilePic from "../../images/profilePic.jpg";
 
-// import ProfileModal from "./../profileModal/profileModal";
 import getProfile from "../../utils/get/getProfile";
 
-const Header = ({ setPerson, setView, person }) => {
-	const [profile, setProfile] = React.useState(null);
+const ProfileModal = ({ profile }) => {
+	return (
+		<Popup
+			trigger={
+				<button
+					type="submit"
+					className={styles["submit-button"]}
+					id="submit-search-name"
+				>
+					Search
+				</button>
+			}
+			modal
+		>
+			{profile
+				? (close) => (
+						<div className={styles["modal-container"]}>
+							{" "}
+							<button
+								onClick={close}
+								className={styles["close-button"]}
+							>
+								&times;
+							</button>
+							<p className={styles["person-name"]}>
+								{profile.name}
+							</p>
+							<img
+								src={profilePic}
+								className={styles["profile-picture"]}
+								alt="searched person"
+							></img>
+							<p className={styles["person-job"]}>
+								{profile.job}
+							</p>
+							<p className={styles["person-office"]}>
+								{profile.office}
+							</p>
+							<ul className={styles["modal-interests"]}>
+								<h3 className={styles["interests-heading"]}>
+									Interests
+								</h3>
+								<li>Eating Out</li>
+								<li>Saying That's Wild</li>
+							</ul>
+							{/* <p>{profile.topics}</p> */}
+							{/* also here we need to have the interests but after we have done some backend stuff to filter that */}
+						</div>
+				  )
+				: null}
+		</Popup>
+	);
+};
+const SearchProfileForm = ({ setPerson, ...props }) => {
 	const [nameSearched, setNameSearched] = React.useState(null);
-
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		setPerson(nameSearched);
 		setNameSearched("");
 	};
-	React.useEffect(() => {
-		setProfile(getProfile(person));
-	}, [person]);
-
+	return (
+		<form onSubmit={handleSubmit} className={styles["person-form"]}>
+			<label htmlFor="search-name" className={styles["input-label"]}>
+				<input
+					onChange={(e) => setNameSearched(e.target.value)}
+					className={styles["name-input"]}
+					id="search-name"
+					value={nameSearched}
+				></input>
+			</label>
+			{props.children}
+		</form>
+	);
+};
+const Header = ({ setPerson, setView, person }) => {
+	const [profile, setProfile] = React.useState(null);
 	const returnToHome = (e) => {
 		e.preventDefault();
 		setView("category");
 	};
-
+	React.useEffect(() => {
+		setProfile(getProfile(person));
+	}, [person, setProfile]);
+	const BeameryLogo = () => (
+		<img src={logo} alt="Beamery Logo" className={styles["logo"]} />
+	);
+	const LogoContainer = ({ ...props }) => (
+		<div className={styles["logo-title-container"]} onClick={returnToHome}>
+			{props.children}
+		</div>
+	);
 	return (
 		<header className={styles["header-container"]}>
-			<div
-				className={styles["logo-title-container"]}
-				onClick={returnToHome}
-			>
-				<img
-					src={logo}
-					alt="Beamery Logo"
-					className={styles["logo"]}
-				></img>
-
+			<LogoContainer>
+				<BeameryLogo />
 				<h1 className={styles["header-title"]}>Connect</h1>
-			</div>
-			<form onSubmit={handleSubmit} className={styles["person-form"]}>
-				<label htmlFor="search-name" className={styles["input-label"]}>
-					Search for a person:
-					<input
-						onChange={(e) => setNameSearched(e.target.value)}
-						className={styles["name-input"]}
-						id="search-name"
-						value={nameSearched}
-					></input>
-				</label>
-
-				<Popup
-					trigger={
-						<button
-							type="submit"
-							className={styles["submit-button"]}
-							id="submit-search-name"
-						>
-							Search
-						</button>
-					}
-					modal
-				>
-					{profile
-						? (close) => (
-								<div className={styles["modal-container"]}>
-									{" "}
-									<button
-										className={styles["close-button"]}
-										onClick={close}
-									>
-										&times;
-									</button>
-									<p className={styles["person-name"]}>
-										{profile.name}
-									</p>
-									<img
-										src={profilePic}
-										className={styles["profile-picture"]}
-										alt="searched person"
-									></img>
-									<p className={styles["person-job"]}>
-										{profile.job}
-									</p>
-									<p className={styles["person-office"]}>
-										{profile.office}
-									</p>
-									<ul className={styles["modal-interests"]}>
-										<h3
-											className={
-												styles["interests-heading"]
-											}
-										>
-											Interests
-										</h3>
-										<li>Eating Out</li>
-										<li>Saying That's Wild</li>
-									</ul>
-									{/* <p>{profile.topics}</p> */}
-									{/* also here we need to have the interests but after we have done some backend stuff to filter that */}
-								</div>
-						  )
-						: null}
-				</Popup>
-			</form>
+			</LogoContainer>
+			<SearchProfileForm setPerson={setPerson}>
+				<ProfileModal profile={profile}></ProfileModal>
+			</SearchProfileForm>
 		</header>
 	);
 };
-
 export default Header;
