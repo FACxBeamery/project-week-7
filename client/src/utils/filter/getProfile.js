231d48ebaf669cd28983819f1824b2769e3faa4e
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-11-06 14:23:33 +0000

    fix filtering utils fns

--- client/src/utils/filter/getProfile.js
+++ client/src/utils/filter/getProfile.js
@@ -1,10 +1,18 @@
 const getProfile = (name, peopleData, topicData) => {
     //change this to not case sensitive
-    const personObj = peopleData.find((obj) => obj.name === name);
-    const topicsArray = personObj.topics.map(
-        (topicID) => topicData.find((topic) => topic._id === topicID).topic
+    const personObj = peopleData.find(
+        (obj) => obj.name.toUpperCase() === name.toUpperCase()
     );
-    return [personObj, topicsArray];
+
+    if (personObj) {
+        const topicsArray = personObj["topics"].map(
+            (id) => topicData.find((topic) => topic._id.equals(id)).topic
+        );
+
+        return [personObj, topicsArray];
+    }
+
+    return null;
 };
 
 export default getProfile;
