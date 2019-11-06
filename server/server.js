9c919ac9c30ed561c21e8908d24683ccc6db5182
lyndseysc <lyndsey.736@gmail.com>
2019-11-06 11:34:46 +0000

    add db init

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const router = require("./router.js");
 const bodyParser = require("body-parser");
+const mongo = require("mongodb").MongoClient;
+
+
 const port = process.env.PORT || 4000;
 
 
+
 const app = express();
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017";
 
 
 app.use(bodyParser);
 
 app.use(router);
+
+
 
 
 
