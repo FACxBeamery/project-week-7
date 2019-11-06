75ca5da025d01984df5c8f00dc9b8c7d12df6b7e
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-11-06 14:21:36 +0000

    add new person with no interests to dummy data

--- client/src/dummyPeople.js
+++ client/src/dummyPeople.js
@@ -1,21 +1,28 @@
 const ObjectID = require("mongodb").ObjectID;
 const initialDataPeople = [
-	{
-		_id: ObjectID("666f6f2d6261722d71757578"),
-		name: "Lyndsey Scott",
-		job: "Graduate Software Engineer",
-		topics: [
-			ObjectID("507f1f77bcf86cd799439011"),
-			ObjectID("54759eb3c090d83494e2d804")
-		],
-		location: "London"
-	},
-	{
-		_id: ObjectID("0123456789ab0123456789ab"),
-		name: "Martha Lambert",
-		job: "Graduate Software Engineer",
-		topics: [ObjectID("54759eb3c090d83494e2d804")],
-		location: "London"
-	}
+    {
+        _id: ObjectID("666f6f2d6261722d71757578"),
+        name: "Lyndsey Scott",
+        job: "Graduate Software Engineer",
+        topics: [
+            ObjectID("507f1f77bcf86cd799439011"),
+            ObjectID("54759eb3c090d83494e2d804")
+        ],
+        location: "London"
+    },
+    {
+        _id: ObjectID("0123456789ab0123456789ab"),
+        name: "Martha Lambert",
+        job: "Graduate Software Engineer",
+        topics: [ObjectID("54759eb3c090d83494e2d804")],
+        location: "London"
+    },
+    {
+        _id: ObjectID("2123456789ab0123456789ab"),
+        name: "Thomas Kostrzewski",
+        job: "Graduate Software Engineer",
+        topics: [],
+        location: "London"
+    }
 ];
 module.exports = initialDataPeople;
